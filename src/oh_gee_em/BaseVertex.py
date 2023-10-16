048ea1880f91f6edf361ae00bca0911de2441074
nk <nk@pop-os.localdomain>
2023-10-15 23:08:01 -0500

    add class methods for get, create, get_or_create, delete. Add methods for update, save, create on instances

--- src/oh_gee_em/BaseVertex.py
+++ src/oh_gee_em/BaseVertex.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
+
 from typing import Union
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, computed_field
 from pydantic.functional_validators import Annotated, AfterValidator
 from gremlin_python.process.traversal import Cardinality, T, Merge
 from uuid import UUID
+import logging
+
+logger = logging.getLogger(__name__)
 
 def enum_UUID_to_str(value):
     if isinstance(value, T):
@@ -22,31 +27,108 @@ def _ftv(property_map: dict) -> dict:
     Returns:
         dict: The cleaned up property_map
     """
-    property_map[T.id.name] = property_map.pop(T.id, None)
+    if property_map is None:
+        raise Exception("'None' is not of type 'dict'")
+
+    property_map[T.id.name] = str(property_map.pop(T.id, None))
     property_map[T.label.name] = property_map.pop(T.label, None)
     return property_map
     
 class BaseVertex(BaseModel):
     id: Annotated[Union[str, T, UUID], AfterValidator(enum_UUID_to_str)] = None
-    label: Annotated[Union[str, T], AfterValidator(enum_UUID_to_str)] = None
+
+    def __del__(self):
+        logger.warning("This will not delete the record in the database, only the local instance!")
+
+    @computed_field
+    @property
+    def label(self) -> str:
+        return self.__class__.__name__.lower()
+    
+    @label.setter
+    def label(self, value: str) -> None:
+        pass
 
     @classmethod
-    def get_vertex(cls, g, id: str | int):
-        vertex = g.V(id).element_map().next()
-        vertex = _ftv(property_map=vertex)
+    def get_vertex(cls, g, id: str | int) -> BaseVertex:
+        """Get a vertex from the database from the given "id".
+
+        Args:
+            g (GraphTraversalSource): The GraphTraversalSource used to get the vertex.
+            id (str | int): The T.id or id of the vertex in the graph.
+
+        Returns:
+            BaseVertex: A created instance of the class that called this method.
+        """
+        vertex = next(g.V(id).element_map(), None)
+        if vertex is None:
+            return None
+        
+        vertex = _ftv(vertex)
         return cls(**vertex)
 
     @classmethod
-    def create_vertex(cls, g, *args, **kwargs):
-        g.merge_v(kwargs).iterate()
-        return cls(**kwargs)
+    def create_vertex(cls, g, *args, **kwargs) -> BaseVertex:
+        vertex = next(g.merge_v(kwargs).element_map(), None)
+        if not vertex:
+            # raise here instead? creation had to have failed.
+            return None
+        
+        vertex = _ftv(vertex)
+        return cls(**vertex)
 
     @classmethod
-    def get_or_create_vertex(cls, g, *args, id: str | int = None, **kwargs):
+    def get_or_create_vertex(cls, g, *args, id: str | int = None, **kwargs) -> BaseVertex:
         id_map = {}
         if id:
             id_map = {T.id: id}
-        vertex = g.merge_v(id_map).option(Merge.on_create, kwargs).element_map().next()
-        vertex = _ftv(property_map=vertex)
+
+        vertex = next(g.merge_v(id_map).option(Merge.on_create, kwargs).element_map(), None)
+        if not vertex:
+            return None
+        
+        vertex = _ftv(vertex)
         return cls(**vertex)
 
+    @classmethod
+    def delete(cls, g, BaseVertex) -> None:
+        g.V(BaseVertex.id).drop().iterate()
+
+    def update(self, **update_fields) -> BaseVertex:
+        for field, value in update_fields.items():
+            setattr(self, field, value)
+        return self
+
+    def save(self, g, *args, **kwargs) -> BaseVertex:
+        # Need an index.
+        if not getattr(self, "id", None):
+            raise Exception("Must have an id to call this method.")
+
+        # Look up by index, if we matched, update the vertex with values stored in the model.
+        id_map = {T.id: str(self.id)}
+        vertex = next(g.merge_v(id_map).option(Merge.on_match, self.model_dump()).element_map(), None)
+        if not vertex:
+            return None
+
+        # remove the enum keys cause they break python classes
+        vertex = _ftv(vertex)
+
+        # multiple people could change these values at the same time,
+        # so update the model with what source of truth gave back.
+        self.update(**vertex)
+
+        return self
+
+    def create(self, g) -> BaseVertex:
+        id_map = {}
+        if self.id:
+            id_map = {T.id: str(self.id)}
+
+        vertex = next(g.merge_v(id_map).option(Merge.on_create, self.model_dump(exclude_none=True)).element_map(), None)
+        if not vertex:
+            # raise here instead? creation had to have failed.
+            return None
+
+        vertex = _ftv(vertex)
+        self.update(**vertex)
+        return self
--- tests/test_BaseVertex.py
+++ tests/test_BaseVertex.py
@@ -1,10 +1,12 @@
 """Test cases for the __main__ module."""
 import pytest
 
+from uuid import uuid4
 from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.process.graph_traversal import __
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from gremlin_python.process.traversal import Cardinality, T, Merge
+from gremlin_python.process.graph_traversal import GraphTraversalSource
 
 from oh_gee_em import BaseVertex, __main__
 
@@ -14,7 +16,7 @@ class Person(BaseVertex):
     sex: str
 
 @pytest.fixture(scope='session')
-def g():
+def g() -> GraphTraversalSource:
     """Fixture for invoking command-line interfaces."""
     url = 'ws://localhost:8182/gremlin'
     remoteConn = DriverRemoteConnection(url, 'g')
@@ -24,18 +26,154 @@ def g():
     remoteConn.close()
 
 @pytest.fixture(scope="function")
-def reset(g):
+def reset(g) -> None:
     g.V().drop().iterate()
 
-def test_mock_person_gocv(g, reset) -> None:
-    fred = Person.get_or_create_vertex(g, name="fred", age=22, sex="m")
+# ---------------------------------------------------------------------------- #
+#                              @CLASS METHOD TESTS                             #
+# ---------------------------------------------------------------------------- #
+def test_mock_person_create_vertex(g, reset) -> None:
+    fred = Person.create_vertex(g, name="fred", age=22, sex="m")
     assert fred.name == "fred"
     assert fred.age == 22
     assert fred.sex == "m"
 
-def test_mock_person(g, reset) -> None:
-    fred = Person.get_or_create_vertex(g, name="ted", age=11, sex="f")
-    assert fred.name == "ted"
-    assert fred.age == 11
-    assert fred.sex == "f"
+def test_mock_person_get_vertex_dne(g, reset) -> None:
+    fred = Person.get_vertex(g, id=str(uuid4()))
+    assert fred == None
 
+def test_mock_person_get_or_create_vertex_1(g, reset) -> None:
+    """test just the creation part of get or create
+    """
+    fred = Person.get_or_create_vertex(g, name="fred", age=22, sex="m")
+    assert fred.name == "fred"
+    assert fred.age == 22
+    assert fred.sex == "m"
+    assert fred.label == fred.__class__.__name__.lower()
+
+def test_mock_person_get_or_create_vertex_2(g, reset) -> None:
+    """test creating and then getting fred
+    """
+    # create fred
+    fred1 = Person.create_vertex(g, name="fred", age=22, sex="m")
+    assert fred1.name == "fred"
+    assert fred1.age == 22
+    assert fred1.sex == "m"
+
+    # get fred
+    fred2 = Person.get_vertex(g, id=fred1.id)
+    assert fred2.name == "fred"
+    assert fred2.age == 22
+    assert fred2.sex == "m"
+
+def test_mock_person_get_or_create_vertex_2(g, reset) -> None:
+    """test creating and then getting fred
+    """
+    # create fred
+    fred1 = Person.create_vertex(g, name="fred", age=22, sex="m")
+    assert fred1.name == "fred"
+    assert fred1.age == 22
+    assert fred1.sex == "m"
+
+    # get fred
+    fred2 = Person.get_vertex(g, id=fred1.id)
+    assert fred2.name == "fred"
+    assert fred2.age == 22
+    assert fred2.sex == "m"
+
+# ---------------------------------------------------------------------------- #
+#                          REGULAR CLASS METHOD TESTS                          #
+# ---------------------------------------------------------------------------- #
+def test_mock_person_save_1(g, reset) -> None:
+    """test creating and then getting fred
+    """
+    # create fred
+    person = Person.create_vertex(g, name="fred", age=22, sex="m")
+    # update their information
+    person.name = "frederick"
+    person.age = 23
+    # save to graph
+    person.save(g)
+
+    # validate update is correct locally
+    assert person.name == "frederick"
+    assert person.age == 23
+    assert person.sex == "m"
+
+    # validate update is correct when loaded
+    francine = Person.get_vertex(g, id=person.id)
+    assert person.name == "frederick"
+    assert person.age == 23
+    assert person.sex == "m"
+
+def test_mock_person_save_2(g, reset) -> None:
+    """test local updates only pushed on .save()
+    """
+    fred = Person.create_vertex(g, name="fred", age=22, sex="m")
+    evil_fred = Person.get_vertex(g, id=fred.id)
+    
+    fred.name = "frederick"
+
+    evil_fred.name = "evil frederick"
+    evil_fred.age = 10000
+    evil_fred.sex = "evil"
+    evil_fred.save(g)
+    assert evil_fred.name == "evil frederick"
+    assert evil_fred.age == 10000
+    assert evil_fred.sex == "evil"
+    assert fred.name == "frederick"
+
+    fred.save(g)
+    assert fred.name == "frederick"
+    assert fred.age == 22
+    assert fred.sex == "m"
+    assert evil_fred.name == "evil frederick"
+    assert evil_fred.age == 10000
+    assert evil_fred.sex == "evil"
+
+def test_mock_person_update(g, reset) -> None:
+    """test update(**dict) convenince method works 
+    """
+    fred = Person.create_vertex(g, name="fred", age=22, sex="m")
+    
+    fred.update(**{"name":"frederick", "age": 50})
+    assert fred.name == "frederick"
+    assert fred.age == 50
+
+def test_mock_person_create_no_id(g, reset) -> None:
+    """test Person.create() convenince method works with automatic id
+    """
+    fred = Person(name="fred", age=22, sex="m")
+    fred.create(g)
+
+    assert fred.name == "fred"
+    assert fred.age == 22
+    assert fred.sex == "m"
+
+def test_mock_person_create_id_manually_set(g, reset) -> None:
+    """test Person.create() convenince method works with manual id
+    """
+    test_id = str(uuid4())
+    fred = Person(id=test_id, name="fred", age=22, sex="m")
+    fred.create(g)
+
+    assert fred.id == test_id
+    assert fred.name == "fred"
+    assert fred.age == 22
+    assert fred.sex == "m"
+
+def test_mock_person_delete(g, reset) -> None:
+    """test Person.delete() convenince method works
+    """
+    fred = Person(name="fred", age=22, sex="m")
+    fred.create(g)
+    
+    assert fred.name == "fred"
+    assert fred.age == 22
+    assert fred.sex == "m"
+
+    # need to set fred = result so the ref count drops to 0 and this gets collected
+    # not sure there's a way in python to hard delete this reference, with an api like
+    # Person.delete(g, fred), guessing not.
+    fred = Person.delete(g, fred)
+    assert fred == None
